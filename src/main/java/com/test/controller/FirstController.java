c6eff07aad79a46ee0a40af2df4761970c3fd779
apinninti6674 <ajay.pinninti@gmail.com>
2022-12-18 11:38:02 +0530

    first commit

--- src/main/java/com/test/controller/FirstController.java
+++ src/main/java/com/test/controller/FirstController.java
@@ -11,4 +11,10 @@ public class FirstController {
 		
 		return "Hello JavaInUse Called in First Service";
 	}
+	
+	@GetMapping("/")
+	public String sayHello() {
+		
+		return "Hello JavaInUse Called in First Service";
+	}
 }
